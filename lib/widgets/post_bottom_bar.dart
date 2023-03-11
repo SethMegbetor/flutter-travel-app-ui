1fc4be341b15778c8cff0ca24d1fc38e73ce89b5
SethMegbetor <sethmegbetor@gmail.com>
2023-03-11 19:19:51 +0000

    working on the post bottom bar

--- lib/widgets/post_bottom_bar.dart
+++ lib/widgets/post_bottom_bar.dart
@@ -7,6 +7,81 @@ class PostBottomBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+      decoration: BoxDecoration(
+        color: Color(0xFFEDF2F6),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      child: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "City Name, Country",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                          size: 25,
+                        ),
+                        Text(
+                          "4.5",
+                          style: TextStyle(
+                            // fontSize: 23,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(height: 25),
+                Text(
+                  "Mollit elit ea ipsum excepteur officia. Dolor nulla tempor minim cillum anim laboris irure occaecat. Culpa Lorem elit magna excepteur officia. Velit nulla et sunt duis eiusmod proident fugiat pariatur culpa commodo. Consectetur voluptate amet magna ea dolor nostrud. Pariatur ea do sunt non cupidatat laboris excepteur cillum veniam. Fugiat quis magna laboris qui esse enim enim qui anim dolor labore.",
+                  style: TextStyle(color: Colors.black54, fontSize: 16),
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 5),
+                      child: Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            "images/city5.jpg",
+                            fit: BoxFit.cover,
+                            width: 120,
+                            height: 90,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
