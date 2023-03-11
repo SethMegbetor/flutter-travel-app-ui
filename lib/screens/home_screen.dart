0721c4c53f4ffbf4f7cb9bc5e357b0efd8d4ac9d
SethMegbetor <sethmegbetor@gmail.com>
2023-03-11 13:20:36 +0000

    added city images

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,8 +7,107 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(preferredSize: Size.fromHeight(90.0),
-      child: HomeAppBar(),),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(90.0),
+        child: HomeAppBar(),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        child: ListView.builder(
+                            itemCount: 6,
+                            scrollDirection: Axis.horizontal,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  width: 160,
+                                  padding: EdgeInsets.all(20),
+                                  margin: EdgeInsets.only(left: 15),
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(15),
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                          "images/city${index + 1}.jpg"),
+                                      fit: BoxFit.cover,
+                                      opacity: 0.7,
+                                    ),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        alignment: Alignment.topRight,
+                                        child: Icon(
+                                          Icons.bookmark_border_outlined,
+                                          color: Colors.white,
+                                          size: 30,
+                                        ),
+                                      ),
+                                      Spacer(),
+                                      Container(
+                                        alignment: Alignment.bottomLeft,
+                                        child: Text(
+                                          "City Name",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 10),
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 6,
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            "Best Places",
+                            style: TextStyle(),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
